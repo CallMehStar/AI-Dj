f1748d3de943ba2f984d08a16c528d048cd70518
CallMehStar <88277772+CallMehStar@users.noreply.github.com>
2022-04-30 19:58:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+Flowers_song="";
+Missing_Someone_song="";
+
 function setup(){
-    canvas = createCanvas(500, 450);
-    canvas.position(525, 200);
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+    Flowers_song = loadSound("musicflower.mp3");
+    Missing_Someone_song = loadSound("musicmissingsomeone.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
 }
