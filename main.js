7ba4689e9c36b137ec696ec4d0aede42ad12ed28
CallMehStar <88277772+CallMehStar@users.noreply.github.com>
2022-05-01 19:32:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
-Flowers_song="";
-Missing_Someone_song="";
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
-function setup(){
-    canvas = createCanvas(600,530);
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function preload(){
-    Flowers_song = loadSound("musicflower.mp3");
-    Missing_Someone_song = loadSound("musicmissingsomeone.mp3");
+function modelLoaded() {
+    console.log('Posenet is Initialized');
 }
 
-function draw(){
-    image(video,0,0,600,530);
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    }
 }
